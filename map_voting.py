7953900feef8d5d66d5f2996b2d97e22264c9026
Lamibe <43702757+Lamibe@users.noreply.github.com>
2022-07-01 18:58:29 +0200

    Changed vote
    Changed vote to use raw SQL. Pushed it in case someone is interested even if it was done only as a crappy workaround

--- map_voting.py
+++ map_voting.py
@@ -9,19 +9,28 @@ def vote():
     up = request.form['up'] == 'true'
     ip = request.remote_addr
     
-    rowcount = db.session.query(
-        Voted.ip,
-        Voted.map) \
-        .filter_by(ip=ip, map=map)
+    #rowcount = db.session.query(
+    #    Voted.ip,
+    #    Voted.map) \
+    #    .filter_by(ip=ip, map=map)
     
-    if len(rowcount.all()) > 0:
-        return "You have already voted on this map", 403
+    #if len(rowcount.all()) > 0:
+    #    return "You have already voted on this map", 403
 
-    db.session.query(Map.id,  Map.votes) \
-        .filter_by(id=map) \
-        .update({Map.votes: Map.votes + (1 if up else -1)}) 
+    #db.session.query(Map.id,  Map.votes) \
+    #    .filter_by(id=map) \
+    #    .update({Map.votes: Map.votes + (1 if up else -1)}) 
     
-    db.session.add(Voted(ip=ip, map=map))
+    #db.session.add(Voted(ip=ip, map=map))
     
-    db.session.commit()
+    #db.session.commit()
+    
+    #Changed that because it wasn't working when tested it. Feel free to remove it and revert old code if it was working
+    
+    rowcount = db.engine.execute("SELECT * FROM voted WHERE ip = \""+str(ip)+"\" AND map = "+str(map)+";")
+    if rowcount.rowcount > 0:
+        return "You have already voted on this map", 403
+    
+    db.engine.execute("INSERT INTO voted VALUES(\""+str(ip)+"\","+str(map)+");")
+    db.engine.execute("UPDATE maps SET votes = votes "+("+" if up else "-")+" 1 WHERE id="+str(map)+";")
     return "thanks"
